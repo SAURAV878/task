a315e007553018e6a2d4c8f5083190a951879905
Saurav <ghimiresaurav789@gmail.com>
2026-02-01 13:48:22 +0545

    feat:learn

--- app.js
+++ app.js
@@ -1,31 +1,20 @@
-console.log('print hey its my frist project');
+const tasks = [
+    
+]
 
-const add = (level) => {
-    console.log('my name is saurav ghimire'+ level);
+
+const addtask = (title, prioriy) => {
+
+    const newtask = {id : Date.now(), title, prioriy, status:'pendind'};
+    tasks.push(newtask);
+  
+
 }
 
-const task = (priorty) => {
-    console.log('i am BCA studnet');
-    add(priorty);
-    console.log('i am in 7th semster');
-}
 
-task('in xavier');
-console.log('finsh');
-console.log("1: User clicks 'Save Task' button");
-
-// Simulate saving to a database (takes 2 seconds)
-setTimeout(() => {
-    console.log("2: Task successfully saved to database!");
-}, 2000);
-
-console.log("3: UI shows a 'Loading...' spinner");
+addtask()
+addtask('hhh', 'k')
+addtask('learn j', 'high');
 
 
-let task1 = { title: null };
-let task2 = task1; 
-
-task2.title = "Buy Bread";
-
-console.log("Task 1 title:", task1.title); 
-console.log("Task 2 title:", task2.title);
+console.log(tasks)
