bcf3794ee6b509f35d5212dd19ccb793cf1e0f58
Saurav <ghimiresaurav789@gmail.com>
2026-02-01 11:56:12 +0545

    feat: leraning frist how they works

--- app.js
+++ app.js
@@ -12,3 +12,20 @@ const task = (priorty) => {
 
 task('in xavier');
 console.log('finsh');
+console.log("1: User clicks 'Save Task' button");
+
+// Simulate saving to a database (takes 2 seconds)
+setTimeout(() => {
+    console.log("2: Task successfully saved to database!");
+}, 2000);
+
+console.log("3: UI shows a 'Loading...' spinner");
+
+
+let task1 = { title: null };
+let task2 = task1; 
+
+task2.title = "Buy Bread";
+
+console.log("Task 1 title:", task1.title); 
+console.log("Task 2 title:", task2.title);
