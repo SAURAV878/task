e03823ee79e336e572fd074b0d31a7d44d01cce3
Saurav <ghimiresaurav789@gmail.com>
2026-02-01 11:40:14 +0545

    feat: bulding a project taskmanager

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log('print hey its my frist project');
+
+const add = (level) => {
+    console.log('my name is saurav ghimire'+ level);
+}
+
+const task = (priorty) => {
+    console.log('i am BCA studnet');
+    add(priorty);
+    console.log('i am in 7th semster');
+}
+
+task('in xavier');
+console.log('finsh');
