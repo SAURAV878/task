69214b618315ebd4217f199dbed087c3232441f5
Saurav <ghimiresaurav789@gmail.com>
2026-02-02 13:23:19 +0545

    feat: leran about callstack memory heap

--- app.js
+++ app.js
@@ -1,20 +1,79 @@
-const tasks = [
-    
+let tasks = [
+
 ]
 
+const createTask = (title, priority, category, dueDate) => {
+    const newTask ={
+        id : Date.now(),
+        title: title,
+        priority: priority,
+        category: category,
+        dueDate: dueDate,
+        completed: false
+    };
+    tasks.push(newTask);
+}
 
-const addtask = (title, prioriy) => {
+const deleteTask = (idTodelete) => {
+    tasks = tasks.filter(t => t !== idTodelete);
+}
 
-    const newtask = {id : Date.now(), title, prioriy, status:'pendind'};
-    tasks.push(newtask);
-  
+const editTask = (id, newTitle) => {
+    const taskToedit = tasks.find(t => t.id === id)
+    if (taskToedit) {
+        taskToedit.title = newTitle
+    }
+}
+
+const getAnalytics = () => {
+    const analytics = tasks.reduce((acc, task) => {
+        acc.total = acc.total + 1;
+
+        if(task.completed === true) {
+            acc.completedCount = acc.completedCount + 1;
+        }
+
+        if(task.priority === 'high') {
+            acc.highCount = acc.highCount +1;
+        }
+
+        return acc;
+    }, {
+        total: 0,
+        completedCount: 0,
+        highCount: 0
+    });
+
+    return analytics;
 
 }
 
+const searchTasks = (term) => {
+    const lowerTerm = term.toLowerCase();
 
-addtask()
-addtask('hhh', 'k')
-addtask('learn j', 'high');
+    return tasks.filter (t => t.title.toLowerCase().includes(lowerTerm));
+}
 
 
-console.log(tasks)
+let searchTimer = null;
+
+const debouncedSearch = (term) => {
+    if(searchTimer != null) {
+        clearTimeout(searchTimer);
+    }
+
+    searchTimer = setTimeout(() => {
+        const results = searchTasks(term);
+        console.log(results);
+
+}, 5000);
+}
+
+
+// const saveToDisk = () => {
+//     JSON.stringify(tasks);
+//     console.log(saveToDisk);
+
+// }
+
+
