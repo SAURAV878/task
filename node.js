b9fea046eb53784333759480ffcc2a0759cb2412
Saurav <ghimiresaurav789@gmail.com>
2026-02-03 13:05:16 +0545

    feat: leran ESmodules import and export

---
+++ node.js
@@ -0,0 +1,18 @@
+// console.log(global);
+
+// console.log(process.version);
+// console.log(process.platform);
+
+
+// const userName = process.argv[2];
+
+// if(!userName) {
+//     console.log('please provide your name');
+// }  else {
+//     console.log('hello ' + userName + ' welcome to node.js')
+// }
+
+import {add, subtract} from './math.js';
+
+console.log(add(5 , 5));
+console.log(subtract(10,5));
